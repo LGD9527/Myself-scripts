4e98b6658dce851bb9141a9285d2c6d89364e1ca
Albrt9527 <69362902+LGD9527@users.noreply.github.com>
2020-11-18 23:00:45 +0800

    Update jdgc1.js

--- jdgc1.js
+++ jdgc1.js
@@ -35,7 +35,6 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
             console.log(`\n===============开始【京东账号${$.UserName}】==================\n`);
             $.errorMsg = '';
             $.index = i + 1;
-            await jdFactory();
             await collectElectricity();
           //await showMsg();
         }
