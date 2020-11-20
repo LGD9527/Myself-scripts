5e49c0d02639c93058369b1cf623c10cd41c4287
Albrt9527 <69362902+LGD9527@users.noreply.github.com>
2020-11-20 20:48:57 +0800

    Update jxgcwzb.js

--- jxgcwzb.js
+++ jxgcwzb.js
@@ -76,7 +76,7 @@ async function jdDreamFactory() {
   await getUserElectricity();
   await taskList();
   //await investElectric();
-  await assistFriend('gB99tYLjvPcEFloDgamoBw==');
+  await assistFriend('bpi9zb_MrMJCEsnZTR8O7w==');
   await hireAward();
   await stealFriend();
   await showMsg();
